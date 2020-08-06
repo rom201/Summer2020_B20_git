935a6aea3816df3cd8fe6f7cf41741ac17a90570
rom201 <68832867+rom201@users.noreply.github.com>
2020-08-05 21:32:48 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,8 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Some code");
+        System.out.println("Some code1");// gitHub
+        System.out.println("Some code2");// gitHub
 
     }
 }
